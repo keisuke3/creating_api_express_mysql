1ebcba8bea62c5f47a64f2c896f60170087a7d95
ユーザー名 <ka.9na3fh.h@gmail.com>
2019-04-13 18:03:07 +0900

    create client

---
+++ client/src/components/Top.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script>
+export default {
+  name: "Top"
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+</style>
---
+++ client/src/store.js
@@ -0,0 +1,12 @@
+import 'babel-polyfill'
+import Vue from 'vue'
+import Vuex from 'vuex'
+import router from './router/index'
+
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  
+})
+
+export default  store
