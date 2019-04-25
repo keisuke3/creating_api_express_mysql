1b128a7fd14d03d8c3ca6ae6e8dd41bc1f85155a
keisuke <ka.9na3fh.h@gmail.com>
2019-04-25 23:38:16 +0900

    update NewTodo.spec.js

--- client/test/unit/specs/NewTodo.spec.js
+++ client/test/unit/specs/NewTodo.spec.js
@@ -6,7 +6,7 @@ const localVue = createLocalVue()
 
 localVue.use(Vuex)
 
-describe('Textarea.vue', () => {
+describe('NewTodo.vue', () => {
   let actions
   let store
   beforeEach(() => {
@@ -19,8 +19,8 @@ describe('Textarea.vue', () => {
   })
   it('actions test', () => {
     const wrapper = shallowMount(NewTodo, {store, localVue})
-    wrapper.find('[new-title]').setValue('testTitle')
-    wrapper.find('[new-body]').setValue('testBody')
+    wrapper.find('input').setValue('testTitle')
+    wrapper.find('textarea').setValue('testBody')
     const button = wrapper.find('button')
     button.trigger('click')
     expect(actions.postTodo).toHaveBeenCalledWith(
@@ -30,12 +30,29 @@ describe('Textarea.vue', () => {
     )
   })
 
-  it('error message test', () => {
+  it('error if title and body are not input', () => {
     const wrapper = shallowMount(NewTodo, {store, localVue})
     expect(wrapper.find('.error-msg').exists()).toBeFalsy()
     const button = wrapper.find('button')
     button.trigger('click')
-    expect(wrapper.find('.error-msg').exists()).toBeTruthy()
-    expect(wrapper.find('.error-msg').text()).toEqual('入力は必須です')
+    expect(wrapper.find('.error-msg').text()).toEqual('タイトル・コメントは入力必須です')
+  })
+
+  it('error if body is not entered', () => {
+    const wrapper = shallowMount(NewTodo, {store, localVue})
+    wrapper.find('input').setValue('testTitle')
+    expect(wrapper.find('.error-msg').exists()).toBeFalsy()
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.find('.error-msg').text()).toEqual('コメントは入力必須です')
+  })
+
+  it('error if title is not entered', () => {
+    const wrapper = shallowMount(NewTodo, {store, localVue})
+    wrapper.find('textarea').setValue('testBody')
+    expect(wrapper.find('.error-msg').exists()).toBeFalsy()
+    const button = wrapper.find('button')
+    button.trigger('click')
+    expect(wrapper.find('.error-msg').text()).toEqual('タイトルは入力必須です')
   })
 })
