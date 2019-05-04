c4698d5fa98a648825cc5f56dc47a94626a4da8a
keisuke <ka.9na3fh.h@gmail.com>
2019-05-04 16:01:25 +0900

    Update Top.spec.js

--- client/test/unit/specs/Top.spec.js
+++ client/test/unit/specs/Top.spec.js
@@ -39,4 +39,11 @@ describe('Top.vue', () => {
       undefined
     )
   })
+  
+  it('The editItems method', () => {
+    const wrapper = shallowMount(Top, {store, localVue})
+    const button = wrapper.find('.edit-button')
+    button.trigger('click')
+    expect(wrapper.vm.editTodo).toEqual({ editId: 1, editTitle: 'testTitle', editBody: 'testBody'})
+  })
 })
